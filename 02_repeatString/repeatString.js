7aefb79cb1ff3700cf4a08123c4ece03f8047602
Bashnu <25891514+Bashnu@users.noreply.github.com>
2024-02-29 19:00:13 +1000

    Loop which passes the tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+string = 'hey';
+if (num > 0){
+    return string.repeat(num);
+} else if  (num < 0) {
+    return 'ERROR';
+} else {
+    return "";
+}
 };
 
 // Do not edit below this line
