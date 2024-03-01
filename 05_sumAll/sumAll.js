fc28e4b4503e4889774e809e3eacd883bd3a18cb
Bashnu <25891514+Bashnu@users.noreply.github.com>
2024-03-01 12:11:13 +1000

    Passed tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    let smallestNum, biggestNum;
+    
+    if(!Number.isInteger(num1) ||!Number.isInteger(num2) || num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    if (num1 < num2) {
+        smallestNum = num1;
+        biggestNum = num2;
+    } else {
+        smallestNum = num2;
+        biggestNum = num1;
+    }
+    sum = 0
 
+    for (let i = smallestNum; i <= biggestNum; i++) {
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
